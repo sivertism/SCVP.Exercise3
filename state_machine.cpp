7be538d7e539af85a36d80f2b0cff4fb700382a1
sivertism <sivert.sliper@lyse.net>
2018-01-23 08:11:23 +0100

    Finished.

--- state_machine.cpp
+++ state_machine.cpp
@@ -1,4 +1,41 @@
 #include "state_machine.h"
 #include <unistd.h>
+#include <sstream>
 
-// TODO
+void stateMachine::process()
+{
+    static int unsigned cur_pos_, matches_;
+
+    cur_pos_ ++;
+    base next_state_ = Start; // Default
+    switch(state)
+    {
+        case Start:
+            if (input.read() == 'G') next_state_ = G;
+            break;
+        case G:
+            if (input.read() == 'A'){
+                next_state_ = GA;
+            } else if (input.read() == 'G') {
+                next_state_ = G;
+            }
+            break;
+        case GA:
+            if (input.read() == 'A'){
+                next_state_ = GAA;
+            } else if (input.read() == 'G') {
+                next_state_ = G;
+            }
+            break;
+        case GAA:
+            if (input.read() == 'G') next_state_ = GAAG;
+            break;
+        case GAAG:
+            if (input.read() == 'G') next_state_ = Start;
+            matches_ ++;
+            std::cout << "Match number " << matches_ << " at character# " << cur_pos_ << std::endl;
+            break;
+    }
+    state = next_state_;
+}
+
--- state_machine.h
+++ state_machine.h
@@ -3,7 +3,24 @@
 
 #include <systemc.h>
 
-// TODO
+SC_MODULE(stateMachine)
+{
+public:
+    sc_in<char> input;
+    sc_in<bool> clk;
+
+    enum base {Start, G, GA, GAA, GAAG};
+    base state;
+
+    SC_CTOR(stateMachine) : input("input"), clk("clk"), state(Start)
+    {
+        SC_METHOD(process);
+        sensitive << clk.pos();
+        dont_initialize();
+    }
+
+    void process( void );
+};
 
 #endif // FSM_H
 
